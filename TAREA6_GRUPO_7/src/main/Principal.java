bf62051ab275e517442d86aad23365d47d216d9b
itati3911 <itati3911@gmail.com>
2025-05-19 20:02:06 -0300

    Se realiza Agregar persona, lógica + vistas

--- TAREA6_GRUPO_7/src/main/Principal.java
+++ TAREA6_GRUPO_7/src/main/Principal.java
@@ -1,4 +1,4 @@
-package main;
+/*package main;
 
 import daoImpl.PersonaDaoImpl;
 import entidad.Persona;
@@ -11,4 +11,33 @@ public class Principal {
 
 	}
 
+}*/
+
+package main;
+
+import negocio.PersonaNegocio;
+import negocioImpl.PersonaNegocioImpl;
+import presentacion.controlador.insert_controller;
+import presentacion.vista.wds_insert;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        //CREO LA VISTA
+        wds_insert ventana = new wds_insert();
+        ventana.setBounds(100, 100, 500, 300); 
+        ventana.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        
+
+        //CAPA DE NEGOCio
+        PersonaNegocio personaNegocio = new PersonaNegocioImpl();
+
+        //CONTROLADOR
+        insert_controller controlador = new insert_controller(ventana, personaNegocio);
+        
+        ventana.setVisible(true); // Mostrar la ventana
+    }
 }
+
+
+
--- TAREA6_GRUPO_7/src/negocioImpl/PersonaNegocioImpl.java
+++ TAREA6_GRUPO_7/src/negocioImpl/PersonaNegocioImpl.java
@@ -2,15 +2,23 @@ package negocioImpl;
 
 import java.util.List;
 
+import dao.PersonaDao;
+import daoImpl.PersonaDaoImpl;
 import entidad.Persona;
 import negocio.PersonaNegocio;
 
 public class PersonaNegocioImpl implements PersonaNegocio {
+	
+	PersonaDao pdao = new PersonaDaoImpl();
 
 	@Override
-	public boolean insert(Persona p) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean insert(Persona p_insert) {
+		boolean estado=false;
+		if(p_insert.getNombre().trim().length()>0 && p_insert.getApellido().trim().length()>0 && p_insert.getDni().trim().length()>0)
+		{
+			estado=pdao.insert(p_insert);
+		}
+		return estado;
 	}
 
 	@Override
@@ -30,5 +38,17 @@ public class PersonaNegocioImpl implements PersonaNegocio {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	@Override
+	public boolean existeDni(String dni) {
+	    List<Persona> personas = pdao.readAll();
+	    for (Persona p : personas) {
+	        if (p.getDni().equals(dni)) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+
 
 }
